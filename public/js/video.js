e31fd26cbaf68c16c5f27951f201016724bbe715
truespring <truespring1@gmail.com>
2022-04-16 20:21:06 +0900

    음소거 컨트롤러 생성 및 볼륨 조절

--- public/js/video.js
+++ public/js/video.js
@@ -17,6 +17,20 @@ function setClock() {
     date_div.innerHTML = `${year} ${month} ${date}`;
 }
 
+function muteController() {
+    const time_zone = document.querySelector('.time-zone');
+    time_zone.addEventListener('click', () => {
+        let video_obj = document.getElementById('video_obj');
+        video_obj.volume = 0.3;
+        
+        if (video_obj.muted) {
+            document.getElementById('video_obj').muted = false;
+        } else {
+            document.getElementById('video_obj').muted = true;
+        }
+    });
+}
+
 function modifyNumber(time) {
     if (parseInt(time) < 10){
         return `0${time}`;
@@ -25,5 +39,6 @@ function modifyNumber(time) {
 
 window.onload = () => {
     setClock();
+    muteController();
     setInterval(setClock, 1000);
 }
